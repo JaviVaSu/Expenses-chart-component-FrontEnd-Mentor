04da70005804d29cc1af5ad377e9236c15514131
Javier Alberto Vásquez Suárez <134559483+JaviVaSu@users.noreply.github.com>
2024-05-08 11:49:16 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+    $("#barra1").hover(function(){
+        $("#mon").css("display", "block");
+    },
+    function(){
+        $("#mon").css("display", "none");
+    });
+
+    $("#barra2").hover(function(){
+        $("#tue").css("display", "block");
+    },
+    function(){
+        $("#tue").css("display", "none");
+    });
+
+    $("#barra3").hover(function(){
+        $("#wed").css("display", "block");
+    },
+    function(){
+        $("#wed").css("display", "none");
+    });
+
+    $("#barra4").hover(function(){
+        $("#thu").css("display", "block");
+    },
+    function(){
+        $("#thu").css("display", "none");
+    });
+
+    $("#barra5").hover(function(){
+        $("#fri").css("display", "block");
+    },
+    function(){
+        $("#fri").css("display", "none");
+    });
+
+    $("#barra6").hover(function(){
+        $("#sat").css("display", "block");
+    },
+    function(){
+        $("#sat").css("display", "none");
+    });
+
+    $("#barra7").hover(function(){
+        $("#sun").css("display", "block");
+    },
+    function(){
+        $("#sun").css("display", "none");
+    });
+
+});
